b55c470cdb01c66617e65f771ae9353ef64f7a67
dsa asd <vsae8538@gmail.com>
2019-08-04 13:35:29 +0800

    20190804

---
+++ leetcode/912/problem.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
+class problem {
+    public static void main(String[] args) {
+        System.out.println('Z' - 'A');
+    }
+    //此題目有時間限制
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
