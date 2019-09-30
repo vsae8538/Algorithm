44939fbf13c54e1290a1f589f3612660ded91c0a
dsa asd <vsae8538@gmail.com>
2019-09-30 20:47:46 +0800

    20190927

---
+++ leetcode/21/leetcode.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+class leetcode {
+    public static void main(String[] args) {    
+
+    }
+    /*
+     * Input: 1->2->4, 1->3->4
+     *  Output: 1->1->2->3->4->4
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+		if(l2 == null) return l1;
+		if(l1.val < l2.val){
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} else{
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2;
+		}
+    }
+}
+
