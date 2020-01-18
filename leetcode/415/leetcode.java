763eb83f7cb71accee6385d172fd8625b17e9651
dsa asd <vsae8538@gmail.com>
2020-01-18 13:21:51 +0800

    20200118

---
+++ leetcode/415/leetcode.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+class leetcode {
+    public static void main(String[] args) {
+  
+    }
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;//進位數
+        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
+            int x = i < 0 ? 0 : num1.charAt(i) - '0'; //若超出字串範圍,則填0
+            int y = j < 0 ? 0 : num2.charAt(j) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10; 
+        }
+        return sb.reverse().toString();
+    }
+}
