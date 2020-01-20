70fddb9a5959a4d2ef6874814ea12b22dca1d9c1
dsa asd <vsae8538@gmail.com>
2020-01-20 22:35:15 +0800

    20200120

---
+++ leetcode/232/leetcode.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+class leetcode {
+    public static void main(String[] args) {    
+        System.out.print("Hello World\n");
+    }
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
+
