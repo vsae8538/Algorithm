8a94ce63096e398a0e3d47e3c43ec1e7b06775e8
dsa asd <vsae8538@gmail.com>
2020-01-24 16:25:20 +0800

    20200124

---
+++ leetcode/674/leetcode.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+class leetcode {
+    public static void main(String[] args) {
+        
+    }
+    
+    public int findLengthOfLCIS(int[] nums) {
+        int res = 0, cnt = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0 || nums[i-1] < nums[i]) res = Math.max(res, ++cnt);
+            else cnt = 1;
+        }
+        return res;
+    }
+}   
