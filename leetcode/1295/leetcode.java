c4a3eda45eabe425d09beea5fc9bd05bded3f9ee
dsa asd <vsae8538@gmail.com>
2020-02-07 23:13:41 +0800

    更正上傳錯誤 20200207

--- leetcode/1295/leetcode.java
+++ leetcode/1295/leetcode.java
@@ -19,7 +19,7 @@ class leetcode {
     public int findNumbers(int[] nums) {
         int res = 0;
         for(int i = 0;i < nums.length;i++)
-            if((String.valueOf(i).length()) % 2 == 0) 
+            if(String.valueOf(nums[i]).length() % 2 == 0) 
                 res++;
         return res;
     }
