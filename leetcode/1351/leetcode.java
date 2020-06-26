7e8062c813800fc3ecfba02d3cfffe715761d7d9
vsae8538 <vsae8538@gmail.com>
2020-06-26 16:41:35 +0800

    20200626

---
+++ leetcode/1351/leetcode.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+class leetcode {
+
+
+    public static void main(String[] args) {    
+        
+    }        
+    public int countNegatives(int[][] grid) {
+
+    }
+    
+    // public int countNegatives(int[][] grid) {
+    //     int res = 0;
+    //     for(int i = 0;i < grid.length;i++){
+    //         for(int j = 0;j < grid[i].length;j++){
+    //                 if(grid[i][j] < 0) res++;
+    //         }
+    //     }
+    //     return res;
+    // }
+
+}
+
