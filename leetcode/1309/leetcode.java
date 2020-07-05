0d27b9cf82d5fffd5a0db378427516361468295b
vsae8538 <vsae8538@gmail.com>
2020-07-05 16:05:53 +0800

    20200705

---
+++ leetcode/1309/leetcode.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+class leetcode {
+
+
+    public static void main(String[] args) {    
+        String s = "123";
+        StringBuffer sb = new StringBuffer();
+        sb.append(s);
+        System.out.println(sb.substring(0,2));
+    }
+    //use queue         
+    public String freqAlphabets(String s) {
+        return null;
+    }
+}
+
