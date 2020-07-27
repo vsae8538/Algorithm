630efaf85d44f2f1d6f2ff6cf94c8efbb091f0f0
vsae8538 <vsae8538@gmail.com>
2020-07-27 18:03:33 +0800

    20200727

---
+++ leetcode/1470/leetcode.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = [0] * len(nums)
+        k = 0
+        j = n
+        for i in range(0,n,1):
+            res[k] = nums[i]
+            res[k+1] = nums[j]
+            k += 2
+            j += 1
+        return res
