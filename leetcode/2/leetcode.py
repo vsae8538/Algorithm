c84d87a5ba07b26386ae6078ae109f95d3eebe48
vsae8538 <vsae8538@gmail.com>
2020-07-29 14:39:26 +0800

    20200729

---
+++ leetcode/2/leetcode.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        root = pt = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = v2.val
+                v2 = v2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            pt.next = ListNode(val)
+            pt = pt.next
+
+        return root.next
