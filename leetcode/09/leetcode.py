6df133b33c649e8e31b214d7f3a24c9d4f3d6ad3
vsae8538 <vsae8538@gmail.com>
2020-07-29 17:38:59 +0800

    20200729

---
+++ leetcode/09/leetcode.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # if(x < 0):
+        #     return false
+        # else:
+        #     s = str(x)
+        #     v2 = ''
+        #     for i in s[::-1]:
+        #         v2 += i
+        #     if(int(v2) == x):
+        #         return true
+        #     else:
+        #         return false 
+        
+        #return int(str(abs(x))[::-1]) == x
