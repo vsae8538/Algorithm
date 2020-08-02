01501966c7cc5a14e8a399a4687451763b163d3b
vsae8538 <vsae8538@gmail.com>
2020-08-02 19:54:05 +0800

    fix

--- leetcode/1528/leetcode.py
+++ leetcode/1528/leetcode.py
@@ -1,15 +1,6 @@
 class Solution:
-    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
-        if not t1 and not t2:
-            return
-        if not t1:
-            res = t2
-        elif not t2:
-            res = t1
-        else:
-            res = TreeNode(t1.val+t2.val)
-            res.left = self.mergeTrees(t1.left,t2.left)
-            res.right = self.mergeTrees(t1.right,t2.right)
-        
-        return res
-    
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res = [0] * len(s)
+        for i in range(0,len(s), 1):
+            res[indices[i]] = s[i]
+        return ''.join(res)
