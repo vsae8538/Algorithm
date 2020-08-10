4ff1265c7c97da44d86d5608f98cb5d4a6972a50
vsae8538 <vsae8538@gmail.com>
2020-08-10 23:11:29 +0800

    20200810

--- leetcode/303/leetcode-old.java
+++ leetcode/303/leetcode-old.java
@@ -18,23 +18,20 @@ class leetcode {
     public static void main(String[] args) {    
 
     }
-
-    /**
-     * sum[k] = sum[1...k-1]
-     * sumRange(i,j) = sum[j+1] - sum[i]
-     */
     class NumArray {
 
-        public int[] sum;
+        public int[] arr;
+
         public NumArray(int[] nums) {
-            sum = new int[nums.length+1];
-            for(int i = 0;i < nums.length;i++){
-                sum[i+1] = sum[i] + nums[i];
-            }
+            arr = nums;
         }
         
         public int sumRange(int i, int j) {
-            return sum[j+1] - sum[i];
+            int res = 0;
+            for(int k = i;k <= j;k++){
+                res += arr[k];
+            }
+            return res;
         }
     }
 }
