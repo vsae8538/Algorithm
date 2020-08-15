08ccce0e0f47a5529dc549983cbd34870611b037
vsae8538 <vsae8538@gmail.com>
2020-08-15 23:21:29 +0800

    20200815 tree dfs

---
+++ leetcode/113/leetcode.java
@@ -0,0 +1,48 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+class leetcode {
+
+    public static void main(String[] args) {    
+        
+    }
+    /**
+     * DFS 非Stack
+
+     */
+    public List<List<Integer>> pathSum(TreeNode root, int sum){
+        List<List<Integer>> result  = new LinkedList<List<Integer>>();
+        List<Integer> currentResult  = new LinkedList<Integer>();
+        pathSum(root,sum,currentResult,result);
+        return result;
+    }
+    
+    public void pathSum(TreeNode root, int sum, List<Integer> currentResult,
+            List<List<Integer>> result) {
+    
+        if (root == null)
+            return;
+        currentResult.add(new Integer(root.val));
+        if (root.left == null && root.right == null && sum == root.val) {
+            result.add(new LinkedList(currentResult));
+            currentResult.remove(currentResult.size() - 1);
+            return;
+        } else {
+            pathSum(root.left, sum - root.val, currentResult, result);
+            pathSum(root.right, sum - root.val, currentResult, result);
+        }
+        currentResult.remove(currentResult.size() - 1);
+    }
+
+}
