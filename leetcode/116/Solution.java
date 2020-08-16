0cab13663d06a20e3f03039b25c6c99dba107be5
vsae8538 <vsae8538@gmail.com>
2020-08-16 13:54:18 +0800

    20200816 bfs

--- leetcode/116/Solution.java
+++ leetcode/116/Solution.java
@@ -23,25 +23,51 @@ class Solution {
     /**
      * BFS
      */
+
     public Node connect(Node root) {
-        if(root == null) return null;
-        Queue<Node> queue = new LinkedList<>(); 
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            Queue<Node> tmpQueue = new LinkedList<>(queue);
-            queue = new LinkedList<>(); 
-            while(!tmpQueue.isEmpty()){
-                Node node = tmpQueue.poll();
-                if(!tmpQueue.isEmpty()){
-                    Node next = tmpQueue.peek();
-                    node.next = next;
-                }else{
-                    node.next = null;
-                }                
-                if(node.left != null) queue.offer(node.left);
-                if(node.right != null) queue.offer(node.right);
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> Q = new LinkedList<Node>(); 
+        Q.add(root);
+        while (Q.size() > 0) {
+            int size = Q.size();
+            for(int i = 0; i < size; i++) {
+                Node node = Q.poll();
+                if (i < size - 1) {
+                    node.next = Q.peek();
+                }
+                if (node.left != null) {
+                    Q.add(node.left);
+                }
+                if (node.right != null) {
+                    Q.add(node.right);
+                }
             }
         }
         return root;
     }
+
+
+    // public Node connect(Node root) {
+    //     if(root == null) return null;
+    //     Queue<Node> queue = new LinkedList<>(); 
+    //     queue.offer(root);
+    //     while(!queue.isEmpty()){
+    //         Queue<Node> tmpQueue = new LinkedList<>(queue);
+    //         queue = new LinkedList<>(); 
+    //         while(!tmpQueue.isEmpty()){
+    //             Node node = tmpQueue.poll();
+    //             if(!tmpQueue.isEmpty()){
+    //                 Node next = tmpQueue.peek();
+    //                 node.next = next;
+    //             }else{
+    //                 node.next = null;
+    //             }                
+    //             if(node.left != null) queue.offer(node.left);
+    //             if(node.right != null) queue.offer(node.right);
+    //         }
+    //     }
+    //     return root;
+    // }
 }
