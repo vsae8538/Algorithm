a4825d783da25d532fb22ae7a38f21aee37aca3a
vsae8538 <vsae8538@gmail.com>
2020-08-18 01:00:01 +0800

    20200818

---
+++ leetcode/198/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+class Solution {
+
+    public static void main(String[] args) {    
+    }
+
+    /**
+     *  dp
+     * 
+     *  [2,1,1,2]
+     *  dp:[2,2,3,4]
+     */
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+
+}
