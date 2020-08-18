c4b468c624e885aa759d25876da28ebd5d22831d
vsae8538 <vsae8538@gmail.com>
2020-08-18 17:55:31 +0800

    20200818

---
+++ leetcode/337/Solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+class Solution {
+
+    public static void main(String[] args) {    
+    }
+
+    /*
+        dp
+    */
+
+    public int rob(TreeNode root) {
+        if (root == null) return 0;
+
+        int money = root.val;
+        if (root.left != null) {
+            money += (rob(root.left.left) + rob(root.left.right));
+        }
+    
+        if (root.right != null) {
+            money += (rob(root.right.left) + rob(root.right.right));
+        }
+    
+        return Math.max(money, rob(root.left) + rob(root.right));
+    }
+}
