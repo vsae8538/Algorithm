6bfe6d4efa07b160e3fe65cf6dceb0761246aeff
vsae8538 <vsae8538@gmail.com>
2020-08-19 16:15:56 +0800

    20200819

--- leetcode/513/Solution.java
+++ leetcode/513/Solution.java
@@ -16,20 +16,37 @@ class Solution {
     public static void main(String[] args) {    
     }
 
+    /**
+     * 
+     * 由右至左遍歷
+     * 
+     */
     public int findBottomLeftValue(TreeNode root) {
-        int res = 0;
-        if(root == null) return res;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-          int size = queue.size();
-          for(int i = 0;i < size;i++){
-            TreeNode node = queue.poll();
-            if(i == 0) res = node.val;
-            if(node.left != null) queue.offer(node.left);
-            if(node.right != null) queue.offer(node.right);
-          }
+      if(root == null) return 0;
+      Queue<TreeNode> queue = new LinkedList<>();
+      queue.offer(root);
+      while(!queue.isEmpty()){
+          root = queue.poll();
+          if(root.right != null) queue.offer(root.right);
+          if(root.left != null) queue.offer(root.left);
         }
-        return res;
-    }
+      return root.val;
+  }
+
+    // public int findBottomLeftValue(TreeNode root) {
+    //     int res = 0;
+    //     if(root == null) return res;
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.offer(root);
+    //     while(!queue.isEmpty()){
+    //       int size = queue.size();
+    //       for(int i = 0;i < size;i++){
+    //         TreeNode node = queue.poll();
+    //         if(i == 0) res = node.val;
+    //         if(node.left != null) queue.offer(node.left);
+    //         if(node.right != null) queue.offer(node.right);
+    //       }
+    //     }
+    //     return res;
+    // }
 }
