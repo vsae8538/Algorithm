282668025e8d550a6e5e29036450905919cde4f6
vsae8538 <vsae8538@gmail.com>
2020-08-21 20:27:12 +0800

    20200821

--- leetcode/1347/Solution.java
+++ leetcode/1347/Solution.java
@@ -14,26 +14,38 @@ import java.util.Stack;
 class Solution {
 
 
+
+    public int minSteps(String s, String t) {
+        int n = s.length(), ans = 0;
+        int[] arr = new int[26];
+        for(int i = 0; i < n; i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < 26; i++) if(arr[i] > 0) ans += arr[i];
+        return ans;
+    }
+
     /**
      * T:39%
      * S:24%
      * 
      * 
      */
-    public int minSteps(String s, String t) {
-        int[] sa = new int[26];
-        for(char c : s.toCharArray()) sa[c - 'a']++;
+    // public int minSteps(String s, String t) {
+    //     int[] sa = new int[26];
+    //     for(char c : s.toCharArray()) sa[c - 'a']++;
         
-        int cnt = 0;
-        for(int j = 0;j < t.length();j++){
-            char c = t.charAt(j);
-            if(sa[c - 'a'] > 0){                
-                sa[c - 'a']--;
-                cnt++;
-            }    
-        }
-        return t.length() - cnt;
-    }
+    //     int cnt = 0;
+    //     for(int j = 0;j < t.length();j++){
+    //         char c = t.charAt(j);
+    //         if(sa[c - 'a'] > 0){                
+    //             sa[c - 'a']--;
+    //             cnt++;
+    //         }    
+    //     }
+    //     return t.length() - cnt;
+    // }
 
     /**
      * 
