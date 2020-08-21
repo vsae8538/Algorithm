b57c37fd56710d2961b816af97f3ff0198f982a9
vsae8538 <vsae8538@gmail.com>
2020-08-21 19:31:18 +0800

    20200821

--- leetcode/1436/Solution.java
+++ leetcode/1436/Solution.java
@@ -13,24 +13,44 @@ import java.util.Stack;
 
 class Solution {
 
-    public String destCity(List<List<String>> paths) {
-        if(paths == null) return null;
-        String res = paths.get(0).get(paths.get(0).size()-1);
-        for(int i = 0;i < paths.size();i++){
-            List<String> prev = paths.get(i);
-            String city = prev.get(prev.size()-1);
-            int st = 0;
-            for(int j = 0;j < paths.size();j++){
-                List<String> curr = paths.get(j);
-                String from = curr.get(0);
-                if(city.equals(from)){
-                    st = 1;
-                    break;
-                }
-            }
-            if(st == 0) res = city;
-        }
 
-        return res;
+
+    public String destCity(List<List<String>> paths) {
+        Map<String, String> map = prepare(paths);
+        String from = paths.get(0).get(0);
+        while(true){
+            if(!map.containsKey(from))
+                return from;
+            from = map.get(from);
+        }
     }
+
+    private Map<String, String> prepare(List<List<String>> paths){
+        Map<String, String> map = new HashMap<>();
+        for(List<String> path : paths)
+            map.put(path.get(0), path.get(1));
+        return map;
+    }
+
+
+    // public String destCity(List<List<String>> paths) {
+    //     if(paths == null) return null;
+    //     String res = paths.get(0).get(paths.get(0).size()-1);
+    //     for(int i = 0;i < paths.size();i++){
+    //         List<String> prev = paths.get(i);
+    //         String city = prev.get(prev.size()-1);
+    //         int st = 0;
+    //         for(int j = 0;j < paths.size();j++){
+    //             List<String> curr = paths.get(j);
+    //             String from = curr.get(0);
+    //             if(city.equals(from)){
+    //                 st = 1;
+    //                 break;
+    //             }
+    //         }
+    //         if(st == 0) res = city;
+    //     }
+
+    //     return res;
+    // }
 }
