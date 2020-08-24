6e4829114001737c9da2343aeb327f210445a19e
vsae8538 <vsae8538@gmail.com>
2020-08-24 19:24:15 +0800

    20200824

--- leetcode/919/Solution.java
+++ leetcode/919/Solution.java
@@ -13,37 +13,77 @@ import java.util.Stack;
 
 class Solution {
 
+    /**
+     * 雙端Queue
+     */
 
     TreeNode root;
+    Deque<TreeNode> deque;
     public CBTInserter(TreeNode root) {
-        if(root == null) return;
         this.root = root;
-    }
-    
-    public int insert(int v) {
-        Queue<TreeNode> queue = new LinkedList<>();
+        deque = new LinkedList();
+        Queue<TreeNode> queue = new LinkedList();
         queue.offer(root);
-        TreeNode parentNode = null;
-        while(!queue.isEmpty()){
+
+        while (!queue.isEmpty()) {
             TreeNode node = queue.poll();
-            if(node.left != null) queue.offer(node.left);
-            if(node.right != null) queue.offer(node.right);
-            if(node.left == null || node.right == null){
-                parentNode = node;
-                break;
-            }
+            if (node.left == null || node.right == null)
+                deque.offerLast(node);
+            if (node.left != null)
+                queue.offer(node.left);
+            if (node.right != null)
+                queue.offer(node.right);
         }
-        if(parentNode.left == null){
-            parentNode.left = new TreeNode(v);
-        }else if(parentNode.right == null){
-            parentNode.right = new TreeNode(v);
-        }
-        return parentNode.val;
     }
-    
+
+    public int insert(int v) {
+        TreeNode node = deque.peekFirst();
+        deque.offerLast(new TreeNode(v));
+        if (node.left == null)
+            node.left = deque.peekLast();
+        else {
+            node.right = deque.peekLast();
+            deque.pollFirst();
+        }
+
+        return node.val;
+    }
+
     public TreeNode get_root() {
         return root;
     }
+
+
+    // TreeNode root;
+    // public CBTInserter(TreeNode root) {
+    //     if(root == null) return;
+    //     this.root = root;
+    // }
+    
+    // public int insert(int v) {
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.offer(root);
+    //     TreeNode parentNode = null;
+    //     while(!queue.isEmpty()){
+    //         TreeNode node = queue.poll();
+    //         if(node.left != null) queue.offer(node.left);
+    //         if(node.right != null) queue.offer(node.right);
+    //         if(node.left == null || node.right == null){
+    //             parentNode = node;
+    //             break;
+    //         }
+    //     }
+    //     if(parentNode.left == null){
+    //         parentNode.left = new TreeNode(v);
+    //     }else if(parentNode.right == null){
+    //         parentNode.right = new TreeNode(v);
+    //     }
+    //     return parentNode.val;
+    // }
+    
+    // public TreeNode get_root() {
+    //     return root;
+    // }
 
 }
 
