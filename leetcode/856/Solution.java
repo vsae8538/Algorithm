6da3274a19cc4a32beb2f50923af2d28cbdc2c6d
vsae8538 <vsae8538@gmail.com>
2020-08-24 23:58:47 +0800

    20200824

---
+++ leetcode/856/Solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+class Solution {
+    
+    public int scoreOfParentheses(String S) {
+        Stack<Integer> stack = new Stack();
+        stack.push(0); 
+    
+        for (char c: S.toCharArray()) {
+            if (c == '(')
+                stack.push(0);
+            else {
+                int v = stack.pop();
+                int w = stack.pop();
+                stack.push(w + Math.max(2 * v, 1));
+            }
+        }
+    
+        return stack.pop();
+    }
+
+
+}
+
+
+
