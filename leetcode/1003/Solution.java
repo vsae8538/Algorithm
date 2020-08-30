da781acff8f24fbee91a0880ceb0eb086162d8f5
vsae8538 <vsae8538@gmail.com>
2020-08-30 12:28:19 +0800

    20200830

---
+++ leetcode/1003/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+class Solution {
+
+    public boolean isValid(String S) {
+        while(S.contains("abc")) {
+            S = S.replaceAll("abc","");
+        }
+        return S.equals("");
+    }
+
+
+}
+
+
+
