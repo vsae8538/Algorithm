b9d59709fa4f8b5e64b468c85d1d4aeef38635b7
vsae8538 <vsae8538@gmail.com>
2020-09-06 17:18:20 +0800

    20200906

---
+++ leetcode/8/Solution.java
@@ -0,0 +1,72 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+
+class Solution {
+
+
+
+    /**
+     * DFA(狀態機)
+     */
+    /**
+     * Such a shitty problem
+
+     */
+    // public int myAtoi(String str) {
+    //     if(str.length() == 0 || str == null) 
+    //         return 0;
+    //     else if(!(str.charAt(0) <= '9' && str.charAt(0) >= '0') && str.charAt(0) != ' '){
+    //         if((str.charAt(0) == '-' || str.charAt(0) == '+') && str.length() > 1){
+    //             if(!(str.charAt(1) <= '9' && str.charAt(1) >= '0')){
+    //                 return 0;
+    //             }
+    //         }else{
+    //             return 0;
+    //         }
+    //     }
+
+    //     int r = 0;
+    //     String s = "";
+    //     while(r < str.length()){
+    //         char c = str.charAt(r);
+    //         if(c >= '0' && c <= '9') s += c;
+    //         else if(c == '-') s += c;
+    //         else if(c == '.') break;
+    //         else if(s.length() > 0 )
+    //         r++;
+    //     }
+    //     if(s.length() == 0) 
+    //         return 0;
+    //     else if(s.length() == 1 && !(s.charAt(0) >= '0' && s.charAt(0) <= '9'))
+    //         return 0;
+
+    //     s = s.charAt(0) + s.substring(1).replace("-", "");
+    //     if(Long.valueOf(s) <= Integer.MIN_VALUE) 
+    //         return Integer.MIN_VALUE;
+    //     else if(Long.valueOf(s) >= Integer.MAX_VALUE) 
+    //         return Integer.MAX_VALUE;
+
+    //     return Integer.valueOf(s);
+    // }
+}
+
+
+
