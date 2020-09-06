8cecf87289e1ce43f88f42e88f265ef7071f45ee
vsae8538 <vsae8538@gmail.com>
2020-09-06 17:41:58 +0800

    20200906

---
+++ leetcode/28/Solution.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+
+class Solution {
+
+
+
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);        
+    }
+
+
+}
+
+
+
