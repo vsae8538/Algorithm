0dd409c0853245a8a2a679b71dc05e11851bcae0
vsae8538 <vsae8538@gmail.com>
2020-09-07 23:13:16 +0800

    20200907

---
+++ leetcode/347/Solution.java
@@ -0,0 +1,61 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+
+class Solution {
+
+
+    /**
+     * Heap 
+     * O(N log k)
+     */
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] m, int[] n) {
+                return m[1] - n[1];
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; ++i) {
+            res[i] = queue.poll()[0];
+        }
+        return res;
+    }
+}
+
+
+
---
+++ leetcode/Tool/Collection.java
@@ -0,0 +1,23 @@
+public class Collection {
+
+    public void tool(){
+        //Heap Desc 
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(int m, int n) {
+                return n - m;
+            }
+        });
+        //Heap Desc 2
+        //PriorityQueue<Integer> queue2 = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+
+
+
+
+
+
+
+    }
+
+
+}
