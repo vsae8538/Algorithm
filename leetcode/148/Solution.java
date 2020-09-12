f745bf29753f64002f762b8fdb48ea291a395285
vsae8538 <vsae8538@gmail.com>
2020-09-12 19:34:11 +0800

    20200912

---
+++ leetcode/148/Solution.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+
+class Solution {
+
+
+
+    /**
+     * 24%
+     */
+    // public ListNode sortList(ListNode head) {
+    //     PriorityQueue<Integer> queue = new PriorityQueue<>();
+    //     ListNode pt = head;
+    //     while(pt != null){
+    //         queue.offer(pt.val);
+    //         pt = pt.next;
+    //     }
+    //     ListNode res = new ListNode(0);
+    //     pt = res;
+    //     while(!queue.isEmpty()){
+    //         pt.next = new ListNode(queue.poll());
+    //         pt = pt.next;
+    //     }
+    //     return res.next;
+    // }
+
+
+}
+
+
+
