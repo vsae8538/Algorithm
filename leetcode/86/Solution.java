6e74877be9bd0e47362c15e5d2c068ee9e981bca
vsae8538 <vsae8538@gmail.com>
2020-09-13 12:20:21 +0800

    20200913

--- leetcode/86/Solution.java
+++ leetcode/86/Solution.java
@@ -21,27 +21,53 @@ import java.util.TreeSet;
 
 class Solution {
 
+
+    /**
+     * 雙指針
+     * 
+     */
     public ListNode partition(ListNode head, int x) {
-        if(head == null) return null;
-        ListNode res = new ListNode(0);
-        ListNode curr = res;
-        curr = linearTrackList(head, curr, 1, x);
-        curr = linearTrackList(head, curr, 0, x);
-        return res.next;
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+        after.next = null;
+        before.next = after_head.next;
+
+        return before_head.next;
     }
 
-    public ListNode linearTrackList(ListNode node, ListNode res, int mode, int x){
-        ListNode pt = node;
-        ListNode curr = res;
-        while(pt != null){
-            if((pt.val < x && mode == 1) || (pt.val >= x && mode == 0)){
-                curr.next = new ListNode(pt.val);
-                curr = curr.next;
-            }
-            pt = pt.next;
-        }
-        return curr;
-    }
+    // public ListNode partition(ListNode head, int x) {
+    //     if(head == null) return null;
+    //     ListNode res = new ListNode(0);
+    //     ListNode curr = res;
+    //     curr = linearTrackList(head, curr, 1, x);
+    //     curr = linearTrackList(head, curr, 0, x);
+    //     return res.next;
+    // }
+
+    // public ListNode linearTrackList(ListNode node, ListNode res, int mode, int x){
+    //     ListNode pt = node;
+    //     ListNode curr = res;
+    //     while(pt != null){
+    //         if((pt.val < x && mode == 1) || (pt.val >= x && mode == 0)){
+    //             curr.next = new ListNode(pt.val);
+    //             curr = curr.next;
+    //         }
+    //         pt = pt.next;
+    //     }
+    //     return curr;
+    // }
 
 }
 
