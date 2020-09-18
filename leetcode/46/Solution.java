619b12d93aa568d02dc0483fdbf0f23fff27c572
vsae8538 <vsae8538@gmail.com>
2020-09-18 19:16:44 +0800

    20200918

---
+++ leetcode/46/Solution.java
@@ -0,0 +1,52 @@
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+
+class Solution {
+    
+    /**
+     *
+     * 回溯
+     * 
+     */
+    List<List<Integer>> res = new LinkedList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        backTracking(nums, new LinkedList<>());
+        return res;
+    }   
+    
+    public void backTracking(int[] nums, List<Integer> list){
+        if(list.size() == nums.length){
+            res.add(new LinkedList<>(list));
+            return;
+        }
+        for(int i = 0;i < nums.length;i++){
+            if(list.contains(nums[i]))
+                continue;
+            list.add(nums[i]);
+            backTracking(nums, list);
+            list.remove(list.size()-1);
+        }
+        
+    }
+}
+
+
+
