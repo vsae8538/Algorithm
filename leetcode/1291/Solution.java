cfafc047656ca8728760ff87acdb8023e570292e
vsae8538 <vsae8538@gmail.com>
2020-09-20 15:17:46 +0800

    20200920

---
+++ leetcode/1291/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+class Solution {
+
+    public List<Integer> sequentialDigits(int low, int high) {
+        int[] allNums = {12,23,34,45,56,67,78,89,
+                         123,234,345,456,567,678,789,
+                         1234,2345,3456,4567,5678,6789,
+                         12345,23456,34567,45678,56789,
+                         123456,234567,345678,456789,
+                         1234567,2345678,3456789,
+                         12345678,23456789,
+                         123456789};
+        List<Integer> res = new ArrayList<>();
+        int n = allNums.length;
+        for (int i = 0; i < n; i++) {
+            if(allNums[i] >= low && allNums[i] <= high)
+                res.add(allNums[i]);
+        }
+        return res;
+    }
+
+
+}
+
+
+
