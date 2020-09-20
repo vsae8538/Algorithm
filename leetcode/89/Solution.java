366c4e98245d7269affe0f32783c6bd407cefa5a
vsae8538 <vsae8538@gmail.com>
2020-09-20 23:54:36 +0800

    20200920

---
+++ leetcode/89/Solution.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
+class Solution {
+
+
+
+
+
+
+    /***
+     *       0
+     *    0      1
+     *   0 1    1 0 
+     *  回溯
+     */
+    // List<Integer> res = new LinkedList<>();
+    // public List<Integer> grayCode(int n) {
+    //     backtracking(n, new StringBuffer());
+    //     return res;
+    // }
+
+    // public void backtracking(int n, StringBuffer sb){
+    //     if(n == sb.length()){
+    //         int num = Integer.parseInt(sb.toString(), 2);
+    //         res.add(num);
+    //         return;
+    //     }
+
+    //     for(int i = 0;i <= 1;i++){
+    //         sb.append(i);
+    //         backtracking(n, sb);
+    //         sb.deleteCharAt(sb.length()-1);
+    //     }
+    // }
+}
+
+
+
