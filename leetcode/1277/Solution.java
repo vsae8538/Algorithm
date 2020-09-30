58d731f76077f512324067173d0b36d1e7526b02
vsae8538 <vsae8538@gmail.com>
2020-09-30 16:21:33 +0800

    20200930

---
+++ leetcode/1277/Solution.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+class Solution {
+
+    /**
+     * 
+              [0,1,1,1],
+              [1,1,2,2],
+              [0,1,2,3]
+     */  
+    public int countSquares(int[][] A) {
+        int res = 0;
+        for (int i = 0; i < A.length; ++i) {
+            for (int j = 0; j < A[0].length; ++j) {
+                if (A[i][j] > 0 && i > 0 && j > 0) {
+                    A[i][j] = Math.min(A[i - 1][j - 1], Math.min(A[i - 1][j], A[i][j - 1])) + 1;
+                }
+                res += A[i][j];
+            }
+        }
+        return res;
+    }
+
+}
+
+
+
