1636447d3c9dad27191aca3ae7724983cd1cdf25
vsae8538 <vsae8538@gmail.com>
2020-10-04 16:45:05 +0800

    20201004

---
+++ leetcode/139/Solution.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && wordDict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+        for(boolean v : f) System.out.print(v + " ");
+        return f[s.length()];
+    }
+}
+
+
+
