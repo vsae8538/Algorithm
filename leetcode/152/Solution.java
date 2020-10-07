c4788f3febc2e0c6a696590a1059e7bb90301b94
vsae8538 <vsae8538@gmail.com>
2020-10-07 21:15:21 +0800

    20201007

---
+++ leetcode/152/Solution.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){ 
+              int tmp = imax;
+              imax = imin;
+              imin = tmp;
+            }
+            imax = Math.max(imax*nums[i], nums[i]);
+            imin = Math.min(imin*nums[i], nums[i]);
+            
+            max = Math.max(max, imax);
+        }
+        return max;
+    }
+
+}
+
+
+
