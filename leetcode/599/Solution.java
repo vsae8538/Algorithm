75337b121fa82690c17f70acbb03c230ffaf3850
vsae8538 <vsae8538@gmail.com>
2020-10-19 19:42:21 +0800

    20201019

---
+++ leetcode/599/Solution.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap < String, Integer > map = new HashMap < String, Integer > ();
+        for (int i = 0; i < list1.length; i++)
+            map.put(list1[i], i);
+        List < String > res = new ArrayList < > ();
+        int min_sum = Integer.MAX_VALUE, sum;
+        for (int j = 0; j < list2.length && j <= min_sum; j++) {
+            if (map.containsKey(list2[j])) {
+                sum = j + map.get(list2[j]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[j]);
+                    min_sum = sum;
+                } else if (sum == min_sum)
+                    res.add(list2[j]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+
+}
+
+
+
