2e925c2058501639450a8ce23414e5bf0727719a
vsae8538 <vsae8538@gmail.com>
2020-10-23 16:56:04 +0800

    20201022

---
+++ leetcode/287/Solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+    
+    public int findDuplicate(int[] nums) {
+
+    }
+
+    // public int findDuplicate(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for(int i = 1;i < nums.length;i++){
+    //         if(nums[i] == nums[i-1]) return nums[i];
+    //     }
+    //     return 0;
+    // }
+}
+
+
+
