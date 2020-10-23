276a542e5a95f0973bfd85e99a1075af2ffeaf36
vsae8538 <vsae8538@gmail.com>
2020-10-23 22:44:00 +0800

    20201023

---
+++ leetcode/524/Solution.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+    
+    public boolean isSubsequence(String x, String y) {
+        int j = 0;
+        for (int i = 0; i < y.length() && j < x.length(); i++)
+            if (x.charAt(j) == y.charAt(i))
+                j++;
+        return j == x.length();
+    }
+    public String findLongestWord(String s, List < String > d) {
+        Collections.sort(d, new Comparator < String > () {
+            public int compare(String s1, String s2) {
+                return s2.length() != s1.length() ? s2.length() - s1.length() : s1.compareTo(s2);
+            }
+        });
+        for (String str: d) {
+            if (isSubsequence(str, s))
+                return str;
+        }
+        return "";
+    }
+
+    public boolean isSubsequence(String x, String y) {
+        int j = 0;
+        for (int i = 0; i < y.length() && j < x.length(); i++)
+            if (x.charAt(j) == y.charAt(i))
+                j++;
+        return j == x.length();
+    }
+    public String findLongestWord(String s, List < String > d) {
+        String max_str = "";
+        for (String str: d) {
+            if (isSubsequence(str, s)) {
+                if (str.length() > max_str.length() || (str.length() == max_str.length() && str.compareTo(max_str) < 0))
+                    max_str = str;
+            }
+        }
+        return max_str;
+    }
+
+
+}
+
+
+
