431cab7a6f5897b04f2abd5a7bdcd0eb8dfe43b5
vsae8538 <vsae8538@gmail.com>
2020-10-23 16:53:28 +0800

    20201023

---
+++ leetcode/424/Solution.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+    
+    private int[] map = new int[26];
+
+    public int characterReplacement(String s, int k) {
+        if (s == null) {
+            return 0;
+        }
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = 0;
+        int historyCharMax = 0;
+        for (right = 0; right < chars.length; right++) {
+            int index = chars[right] - 'A';
+            map[index]++;
+            historyCharMax = Math.max(historyCharMax, map[index]);
+            if (right - left + 1 > historyCharMax + k) {
+                map[chars[left] - 'A']--;
+                left++;
+            }
+        }
+        return chars.length - left;
+    }
+
+}
+
+
+
