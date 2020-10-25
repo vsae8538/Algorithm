2d7d7b6e02ad9ce6054550a2eee7e960ffa749c8
vsae8538 <vsae8538@gmail.com>
2020-10-25 21:20:51 +0800

    20201025

---
+++ leetcode/713/Collection.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Collection {
+
+    /**
+     * 
+     * 2 5 6 10
+     * 
+     * 3
+     */
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int prod = 1, ans = 0, left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            prod *= nums[right];
+            while (prod >= k) prod /= nums[left++];
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+
+
+}
