6b32dab3be6616a9886f327b230655d57bc043f9
vsae8538 <vsae8538@gmail.com>
2020-10-26 23:21:17 +0800

    20201026

---
+++ leetcode/925/Solution.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+    /**
+     * "alex"
+       "aaleelx"
+
+     */
+    public boolean isLongPressedName(String name, String typed) {
+        if(typed.charAt(0) != name.charAt(0)) return false;
+        int i = 0, j = 0;
+        int[] map = new int[26];
+        for(char c : name.toCharArray()) map[c-'a']++;
+        for(i = 0;i < typed.length();i++){
+            if(j == name.length()) break;
+            char c = typed.charAt(i);
+            if(c == name.charAt(j)){
+                j++;
+            }else if(map[c-'a'] == 0){
+                return false;
+            }else if(j > 0 && c != name.charAt(j) && c != name.charAt(j-1)){
+                return false;
+        }
+        for(;i < typed.length();i++){
+            char c = typed.charAt(i);
+            if(map[c-'a'] == 0)
+                return false;
+        }
+        return j == name.length();
+    }
+}
+
+
+
