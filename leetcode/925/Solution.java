ea56d3837e1ad228a12983b1f8083c95447a58bd
vsae8538 <vsae8538@gmail.com>
2020-10-26 23:23:57 +0800

    20201026

--- leetcode/925/Solution.java
+++ leetcode/925/Solution.java
@@ -19,6 +19,21 @@ import java.util.TreeSet;
 
 
 class Solution {
+
+    /**
+     * 
+     */
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0, m = name.length(), n = typed.length();
+        for (int j = 0; j < n; ++j)
+            if (i < m && name.charAt(i) == typed.charAt(j))
+                ++i;
+            else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1))
+                return false;
+        return i == m;
+    }
+
+
     /**
      * "alex"
        "aaleelx"
