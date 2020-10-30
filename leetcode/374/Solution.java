e726df49f905e8e939cd83c5dafda62225fdac73
vsae8538 <vsae8538@gmail.com>
2020-10-30 17:41:26 +0800

    20201030

---
+++ leetcode/374/Solution.java
@@ -0,0 +1,48 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+class Solution {
+
+   
+
+    // int guess(int num){
+    //     return num;
+    // }
+
+    public int guessNumber(int n) {
+        int left = 1,right = n;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(guess(mid) == -1){
+                right = mid - 1;
+            }else if(guess(mid) == 1){
+                left = mid + 1;
+            }else if(guess(mid) == 0){
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
