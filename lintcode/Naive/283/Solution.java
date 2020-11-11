98ddf268097b5e5e3fa038b8a812ac59d83297cf
vsae8538 <vsae8538@gmail.com>
2020-11-11 23:25:00 +0800

    20201111

---
+++ lintcode/Naive/283/Solution.java
@@ -0,0 +1,33 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+
+class Solution {   
+    public int maxOfThreeNumbers(int num1, int num2, int num3) {
+        return Math.max(num1,Math.max(num2,num3));
+    }
+
+}
+
+
+
