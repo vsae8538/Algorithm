a9dbb850e07f602fc33c90f6b5915876257cc1db
vsae8538 <vsae8538@gmail.com>
2020-11-12 20:49:08 +0800

    20201112

---
+++ lintcode/Naive/484/Solution.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+
+public class Solution {
+
+    public int countNodes(ListNode head) {
+        if(head == null) return 0;
+        int cnt = 0;
+        for(ListNode pt = head;pt != null;pt = pt.next) 
+            cnt++;
+        return cnt;
+    }
+
+}
+
+
+
