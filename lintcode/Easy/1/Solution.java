94ceee96a7a6b973ec1c03a1de367254b38cd705
vsae8538 <vsae8538@gmail.com>
2020-11-12 21:35:18 +0800

    20201112

---
+++ lintcode/Easy/1/Solution.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+
+public class Solution {
+
+
+    public int aplusb(int a, int b) {
+        if((a & b) == 0) 
+		    return a|b;
+	    return aplusb(a ^ b, (a & b) << 1);
+    }
+
+
+}
+
+
+
