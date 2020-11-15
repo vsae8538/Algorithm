de2283bd7aa8e650874b59b7be31ed0d49c87e58
vsae8538 <vsae8538@gmail.com>
2020-11-15 22:32:50 +0800

    20201115

---
+++ lintcode/Easy/3/Solution.java
@@ -0,0 +1,45 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+class Solution {
+
+
+
+    
+    /**
+     * 暴力
+     */
+    // public int digitCounts(int k, int n) {
+    //     int res = 1;
+    //     String s = "";
+    //     for(int i = 10;i <= n;i++){
+    //         s = String.valueOf(i);
+    //         for(char c : s.toCharArray()){
+    //             if(c - '0' == k)
+    //                 res++;
+    //         }
+    //     }
+    //     return res;
+    // }
+}
