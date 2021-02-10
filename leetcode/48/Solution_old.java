fb9ae7b9c675fe3b43ec0b881a711cdc68d01ead
vsae8538 <vsae8538@gmail.com>
2021-02-10 22:01:03 +0800

    20210210

---
+++ leetcode/48/Solution_old.java
@@ -0,0 +1,48 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+class Solution{
+
+    //輔助數組
+    // public void rotate(int[][] matrix) {
+    //     int m = matrix.length, n = matrix[0].length;
+    //     int[][] aux = new int[m][n];
+    //     int c = n - 1, k = 0; 
+    //     for(int i = 0;i < m;i++){
+    //         for(int j = 0;j < n;j++){
+    //             aux[k][c] = matrix[i][j];
+    //             k++;
+    //         }
+    //         c--;
+    //         k = 0;
+    //     }
+
+    //     for(int i = 0;i < m;i++){
+    //         for(int j = 0;j < n;j++){
+    //             matrix[i][j] = aux[i][j];
+    //         }
+    //     }
+    // }
+
+}
