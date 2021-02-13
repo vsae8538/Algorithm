a230a8031d1ea17a2a8bb7291b504307dd3807e4
vsae8538 <vsae8538@gmail.com>
2021-02-13 14:46:30 +0800

    20210213

---
+++ leetcode/55/Solution 2021.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+class Solution{
+    public boolean canJump(int[] nums){
+        int rightmost = 0, n = nums.length;
+        for(int i = 0;i < n;i++){
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
