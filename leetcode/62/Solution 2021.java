333013c31f5fc4e5a01f8f5041daac61618397e4
vsae8538 <vsae8538@gmail.com>
2021-02-14 12:36:45 +0800

    20210214

---
+++ leetcode/62/Solution 2021.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+class Solution{
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0;i < m;i++) dp[i][0] = 1;
+        for(int i = 0;i < n;i++) dp[0][i] = 1;
+        for(int i = 1;i < m;i++){
+            for(int j = 1;j < n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
