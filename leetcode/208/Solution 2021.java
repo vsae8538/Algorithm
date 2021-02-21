87b4c32ef51e9e8ea402a634bf70eb28b57a1217
vsae8538 <vsae8538@gmail.com>
2021-02-21 10:56:42 +0800

    20210221

---
+++ leetcode/208/Solution 2021.java
@@ -0,0 +1,100 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+class TrieNode{
+    TrieNode[] links;
+
+    private int R = 26;
+
+    boolean isEnd;
+
+    public TrieNode(){
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch){
+        return links[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch){
+        return links[ch - 'a'];
+    }
+
+    public void put(char ch, TrieNode node){
+        links[ch - 'a'] = node;
+    }
+
+    public void setEnd(){ isEnd = true; }
+    
+    public boolean isEnd(){ return isEnd; }
+}
+
+class Trie {
+
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i = 0;i < word.length();i++){
+            char currentChar = word.charAt(i);
+            if(!node.containsKey(currentChar)){
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    public TrieNode searchPrefix(String word){
+        TrieNode node = root;
+        for(int i = 0;i < word.length();i++){
+            char currentChar = word.charAt(i);
+            if(node.containsKey(currentChar)){
+                node = node.get(currentChar);
+            }else{
+                return null;
+            }
+        }
+        return node;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
