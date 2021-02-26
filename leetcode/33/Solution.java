eae3545d863585adf611becadd3141d0578f54fc
vsae8538 <vsae8538@gmail.com>
2021-02-26 16:23:48 +0800

    20210226

--- leetcode/33/Solution.java
+++ leetcode/33/Solution.java
@@ -1,8 +1,11 @@
-import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,28 +18,37 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.Stack;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.Map.Entry;
 
+class Solution{
 
-class Solution {
-
-    /**
-     * 雙指針搜尋
-     * O(logn)
-     */
     public int search(int[] nums, int target) {
-        int left = 0,right = nums.length-1;
-        int st = 0;
+        if(nums.length == 0){ return -1;}
+        if(nums.length == 1){ return nums[0] == target ? 0 : -1;}
+
+        int n = nums.length;
+        int left = 0, right = n - 1;
         while(left <= right){
-            if(left <= right && nums[left] == target) return left;
-            if(left <= right && nums[right] == target) return right;
-            left++;
-            right--;
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[0] <= nums[mid]){
+                if(nums[0] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && target <= nums[n-1]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
         }
+
         return -1;
     }
-
+    
 }
-
-
-
