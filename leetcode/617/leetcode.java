91ecbfed76194bcb101f6046ea0690da74a0ff8b
vsae8538 <vsae8538@gmail.com>
2021-02-26 16:20:44 +0800

    20210226

--- leetcode/617/leetcode.java
+++ leetcode/617/leetcode.java
@@ -13,52 +13,31 @@ import java.util.Set;
 
 class leetcode {
 
+    public int search(int[] nums, int target) {
+        if(nums.length == 0){ return -1;}
+        if(nums.length == 1){ return nums[0] == target ? 0 : -1;}
 
-    public static void main(String[] args) {    
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[0] <= nums[mid]){
+                if(nums[0] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && target <= nums[n-1]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
 
-    }
-    // public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-    //     if(t1 == null && t2 != null){
-    //         t1 = new TreeNode(0);
-    //     }
-    //     else if(t1 == null && t2 == null){
-    //         return t1;
-    //     }
-    //     sumTree(t1, t2);
-    //     return t1;
-    // }
-
-    // public void sumTree(TreeNode t1,TreeNode t2){
-    //     if(t2 == null)
-    //         return;
-    //     else if(t1 == null && t2 == null)
-    //         return;
-
-    //     t1.val += t2.val;
-        
-    //     if(t2 != null){
-    //         if(t1.left == null && t2.left != null){
-    //             t1.left = new TreeNode(0);
-    //         }
-    //         if(t1.right == null && t2.right != null){
-    //             t1.right = new TreeNode(0);
-    //         }
-    //     }
-
-    //     sumTree(t1.left, t2.left);
-    //     sumTree(t1.right, t2.right);
-    // }
-
-
-    public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null)
-            return t2;
-        if (t2 == null)
-            return t1;
-        t1.val += t2.val;
-        t1.left = mergeTrees(t1.left, t2.left);
-        t1.right = mergeTrees(t1.right, t2.right);
-        return t1;
+        return -1;
     }
 }
 
