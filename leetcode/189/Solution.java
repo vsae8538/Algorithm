f3e794cd50b1022c84af186b9560ec67657a09f4
vsae8538 <vsae8538@gmail.com>
2021-04-06 13:14:41 +0800

    20210406

---
+++ leetcode/189/Solution.java
@@ -0,0 +1,55 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+class Solution{
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+
+
+    // public void rotate(int[] nums, int k) {
+    //     int[] res = new int[nums.length];
+    //     for(int i = 0;i < nums.length;i++){
+    //         int index = (i + k) % nums.length;
+    //         res[index] = nums[i];
+    //     }
+        
+    //     for(int i = 0;i < nums.length;i++){
+    //         nums[i] = res[i];
+    //     }
+    // }
+}
