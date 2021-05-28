8de38ae3d3192b95c546b1811e608cb60ff3a9a2
vsae8538 <vsae8538@gmail.com>
2021-05-28 16:59:30 +0800

    20210528

---
+++ leetcode/316/Solution.java
@@ -0,0 +1,57 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+
+class Solution{
+
+    public String removeDuplicateLetters(String s) {
+        Stack<Character> stk = new Stack<>();
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+        }
+
+        boolean[] inStack = new boolean[256];
+        for (char c : s.toCharArray()) {
+            count[c]--;
+    
+            if (inStack[c]) continue;
+    
+            while (!stk.isEmpty() && stk.peek() > c) {
+                if (count[stk.peek()] == 0) {
+                    break;
+                }
+                inStack[stk.pop()] = false;
+            }
+            stk.push(c);
+            inStack[c] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stk.empty()) {
+            sb.append(stk.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
+
