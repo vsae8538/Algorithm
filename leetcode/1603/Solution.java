39e69d6ebd0f9f78212929c28c5c8716670b3f97
vsae8538 <vsae8538@gmail.com>
2021-06-19 18:28:53 +0800

    20210619

---
+++ leetcode/1603/Solution.java
@@ -0,0 +1,52 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.Scanner;
+
+class Solution{
+
+    int big = 0;
+    int medium = 0;
+    int small =0;
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+    
+    public boolean addCar(int carType) {
+        if(carType == 1 && big > 0){
+            big--;
+            return true;
+        }else if(carType == 2 && medium > 0){
+            medium--;
+            return true;
+        }else if(carType == 3 && small > 0){
+            small--;
+            return true;
+        }
+
+        return false;
+    }
+}
