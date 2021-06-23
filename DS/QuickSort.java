f6b070eebe7b4239bbe3947c575f811e0f2e6953
vsae8538 <vsae8538@gmail.com>
2021-06-23 22:02:22 +0800

    change

--- DS/QuickSort.java
+++ DS/QuickSort.java
@@ -2,22 +2,35 @@
  * QuickSort
  */
 public class QuickSort {
+ 
     
-    public static void quickSort(int A[], int left, int right){
-        if(left < right){
-            int i = left,j = right;
-            int k = A[left];
-            while(i < j){
-                while(i < j && A[j] >= k) j--;
-                A[i] = A[j]; 
-                while(i < j && A[i] <= k) i++;
-                A[j] = A[i];
-            }
-            A[i] = k;
-            quickSort(A, left, i - 1);
-            quickSort(A, i + 1 , right);
+    public void quickSort(int a[], int left, int right){
+        if(left <= right){
+            int mid = partiton(a, left, right);
+            quickSort(a, mid + 1, right);
+            quickSort(a, left, mid - 1);
         }
     }
     
+    public int partiton(int[] a, int left, int right){
+        int i = left + 1, j = right, pivot = a[left];
+        while(i <= j){
+            while(i <= j && a[i] <= pivot) i++;
+            while(i <= j && a[j] > pivot) j--;
+            if(i < j){
+                swap(a, i, j);
+            }
+        }
+        
+        swap(a, left, j);
+
+        return j;
+    }
+
+    public void swap(int a[], int i, int j){
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
 }
 
