8714ddcb0a713997fc83d4c997d20086ab6e6975
vsae8538 <vsae8538@gmail.com>
2021-06-23 16:51:08 +0800

    20210623

--- leetcode/494/Solution.java
+++ leetcode/494/Solution.java
@@ -21,27 +21,83 @@ import java.util.Stack;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.Map.Entry;
+import java.util.Random;
 
 class Solution{
 
     /**
-     * dfs O(2^n)
-     */
-    int res = 0;
+     *  dp
+     *  轉化01背包 
+        1.x - (sum - x) = target
+        2.x = target + sum / 2
+        bagSize = target + sum / 2
+    */
     public int findTargetSumWays(int[] nums, int S) {
-        dfs(nums, S, 0, 0);
-        return res;    
-    }
-    
-    public void dfs(int[] nums, int S, int i, int sum){
-        if(i >= nums.length){
-            if(sum == S) 
-                res++;
-            return;
-        }
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(S > sum || (S + sum) % 2 == 1) return 0;
+        int target = (S + sum) / 2;
 
-        dfs(nums, S, i+1, sum + nums[i]);
-        dfs(nums, S, i+1, sum - nums[i]);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for(int num : nums){
+            for(int j = target; j >= num; j--){
+                dp[j] = dp[j] + dp[j - num];
+            }
+        }
+        return dp[target];
     }
+
+
+    /**
+     * 記憶化搜索
+     */
+    // Map<String, Integer> cache = new HashMap<>();
+    // public int findTargetSumWays(int[] nums, int target) {
+    //     return dfs(nums, 0, target);
+    // }
+
+    // public int dfs(int[] nums, int index, int target){
+    //     String key = index + "_" + target;
+    
+    //     if(cache.containsKey(key)){
+    //         return cache.get(key);
+    //     }
+
+    //     if(target == 0 && index == nums.length){
+    //         cache.put(key, 1);
+    //         return 1;
+    //     }
+    //     if(index >= nums.length){
+    //         return 0;
+    //     }       
+
+    //     int sub = dfs(nums, index + 1, target - nums[index]);
+    //     int add = dfs(nums, index + 1, target + nums[index]);
+    //     cache.put(key, add + sub);
+    //     return cache.get(key);
+    // }
+
+    /**
+     * DFS
+
+     */
+    // public int findTargetSumWays(int[] nums, int target) {
+    //     return dfs(nums, 0, target);
+    // }
+
+    // public int dfs(int[] nums, int index, int target){
+    //     if(target == 0 && index == nums.length){
+    //         return 1;
+    //     }
+    //     if(index >= nums.length){
+    //         return 0;
+    //     }       
+
+    //     int sub = dfs(nums, index + 1, target - nums[index]);
+    //     int add = dfs(nums, index + 1, target + nums[index]);
+
+    //     return add + sub;
+    // }
 
 }
