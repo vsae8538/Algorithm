cfce948d48b1b8b590e0c2d2a2ea699325b24362
vsae8538 <vsae8538@gmail.com>
2021-06-26 20:16:19 +0800

    20210626

--- leetcode/509/Solution.java
+++ leetcode/509/Solution.java
@@ -33,6 +33,22 @@ class Solution {
         return dp[N];
     }
 
+    /**
+     * dp
+     * 空間優化
+     */
+    public int fib(int n) {
+        if(n == 0) return 0;
+        int n1 = 0, n2 = 1;
+        for(int i = 2;i <= n;i++){
+            int sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+        }
+
+        return n2;
+    }
+
 
 }
 
