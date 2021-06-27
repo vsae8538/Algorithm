c4c4191db865a46f8387e7363825f015c737ad0d
vsae8538 <vsae8538@gmail.com>
2021-06-27 16:40:45 +0800

    20210627

---
+++ leetcode/121/Solution.java
@@ -0,0 +1,43 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+import java.util.Random;
+
+class Solution{
+
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0; //不持有股票
+        dp[0][1] = -prices[0]; //持有股票
+        for(int i = 1;i < prices.length;i++){
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+        }
+
+        return dp[prices.length-1][0];
+    }
+
+ 
+}
+
+
