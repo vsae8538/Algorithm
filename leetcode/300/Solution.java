d80d01e2659facab66e35deceb5a39f2064a3f01
vsae8538 <vsae8538@gmail.com>
2021-06-28 19:18:17 +0800

    20210628

--- leetcode/300/Solution.java
+++ leetcode/300/Solution.java
@@ -32,24 +32,24 @@ class Solution{
 
     /**
      * dp
-     * @param nums
-     * @return
-     */
+     * */
+
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
         dp[0] = 1;
-        int max = 1;
         for(int i = 1;i < nums.length;i++){
-            dp[i] = 1;
             for(int j = 0;j < i;j++){
-                if(nums[i] > nums[j]){
+                if(nums[j] < nums[i]){
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            max = Math.max(dp[i], max);
         }
 
-        return max;
+        int res = -1;
+        for(int n : dp) res = Math.max(res, n); 
+        
+        return res;
     }
   
 
