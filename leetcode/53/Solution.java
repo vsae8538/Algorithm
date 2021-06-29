762ceb3f6ec04f75ba8054cf139537a7599cdabe
vsae8538 <vsae8538@gmail.com>
2021-06-29 20:09:36 +0800

    20210629

--- leetcode/53/Solution.java
+++ leetcode/53/Solution.java
@@ -25,6 +25,24 @@ import java.util.Map.Entry;
 class Solution{
 
     /**
+     * dp
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length]; 
+        dp[0] = nums[0];
+        int max = Math.max(dp[0], Integer.MIN_VALUE);
+        for(int i = 1;i < nums.length;i++){
+            dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);
+            max = Math.max(dp[i], max);
+        }
+
+        return max;
+    }
+
+    /**
      * 一維DP 同前綴和
      */
     public int maxSubArray(int[] nums) {
