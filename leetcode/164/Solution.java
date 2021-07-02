c59cb082bd003b2db887292700eb55f424d8d51f
vsae8538 <vsae8538@gmail.com>
2021-07-02 15:06:31 +0800

    20210702

--- leetcode/164/Solution.java
+++ leetcode/164/Solution.java
@@ -26,9 +26,41 @@ import java.util.Random;
 class Solution{
 
     /**
-     * 補上桶排序
+     * O(n)
      */
 
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        
+        if(n < 2) return 0;
+        
+        long exp = 1;
+        int[] buf = new int[n];
+        int maxVal = Arrays.stream(nums).max().getAsInt();  
+
+        while(maxVal >= exp){
+            int[] cnt = new int[10];
+            for (int i = 0; i < n; i++) {
+                int digit = (nums[i] / (int) exp) % 10;
+                cnt[digit]++;
+            }
+            for (int i = 1; i < 10; i++) {
+                cnt[i] += cnt[i - 1];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                int digit = (nums[i] / (int) exp) % 10;
+                buf[cnt[digit] - 1] = nums[i];
+                cnt[digit]--;
+            }
+            System.arraycopy(buf, 0, nums, 0, n);
+            exp *= 10;
+        }
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            ret = Math.max(ret, nums[i] - nums[i - 1]);
+        }
+        return ret;
+    }
 
     /**
      * O(nlogn)
