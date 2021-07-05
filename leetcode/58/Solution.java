22f1081f6922ab1628f515ad939da8f2a8fd8356
vsae8538 <vsae8538@gmail.com>
2021-07-05 20:17:47 +0800

    20210705

---
+++ leetcode/58/Solution.java
@@ -0,0 +1,40 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+import java.util.Random;
+
+class Solution{
+
+    public int lengthOfLastWord(String s) {
+        if(s.length() == 0 || s.equals(" ")){
+            return 0;
+        }
+        String[] sp = s.split(" ");
+        return sp.length == 0 ? 0 : sp[sp.length - 1].length();
+    }
+
+
+
+}   
+
+
