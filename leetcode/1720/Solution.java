7c6cbbf14849d736d5330afbc1856ecd28ad755d
vsae8538 <vsae8538@gmail.com>
2021-07-08 22:53:52 +0800

    20210708

---
+++ leetcode/1720/Solution.java
@@ -0,0 +1,41 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+import java.util.Random;
+
+class Solution{
+
+
+    public int[] decode(int[] encoded, int first) {
+        int[] res = new int[encoded.length+1];
+        res[0] = first;
+        for(int i = 1;i < res.length;i++){
+            res[i] = res[i-1] ^ encoded[i-1];
+        }
+
+        return res;
+    }
+
+}   
+
+
